348f096efe4d911b1496a85e6e32c901f9de5a27
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 17:30:00 +0700

    [FEATURE] Remove items from cart

--- BTTH03_javascript/projects/03_advanced/cart.js
+++ BTTH03_javascript/projects/03_advanced/cart.js
@@ -1,3 +1,5 @@
 let cart = []; const products = [{id:1, name:'Laptop', price:1000}];
 
 function addToCart(productId) { cart.push(productId); }
+
+function removeFromCart(productId) { cart = cart.filter(id => id !== productId); }
