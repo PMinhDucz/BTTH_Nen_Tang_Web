8977201dfda91300c27b445d77804908ee965eb3
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 17:10:00 +0700

    [ASYNC] Fetch movie data from OMDB API

--- BTTH03_javascript/projects/03_advanced/movie.js
+++ BTTH03_javascript/projects/03_advanced/movie.js
@@ -1,1 +1,3 @@
 const API_KEY = 'YOUR_KEY'; async function searchMovie(query) {}
+
+async function searchMovie(query) { const res = await fetch(http://www.omdbapi.com/?s=+query); const data = await res.json(); return data; }
