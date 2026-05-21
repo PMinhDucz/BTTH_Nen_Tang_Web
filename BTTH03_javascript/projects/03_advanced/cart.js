a82f5e5e248f7ee77f10411feb68e96eca241d78
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 17:34:00 +0700

    [FEATURE] Calculate subtotal and VAT

--- BTTH03_javascript/projects/03_advanced/cart.js
+++ BTTH03_javascript/projects/03_advanced/cart.js
@@ -3,3 +3,5 @@ let cart = []; const products = [{id:1, name:'Laptop', price:1000}];
 function addToCart(productId) { cart.push(productId); }
 
 function removeFromCart(productId) { cart = cart.filter(id => id !== productId); }
+
+function calcTotal() { return cart.reduce((sum, item) => sum + item.price, 0); }
