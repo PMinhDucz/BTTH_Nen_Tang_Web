b2b784df7a53eeae07e73cad95104b6b8e150a7e
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 15:35:00 +0700

    [UI] Implement Random Color Palette

--- BTTH03_javascript/projects/01_basics/03_mini.js
+++ BTTH03_javascript/projects/01_basics/03_mini.js
@@ -1,2 +1,5 @@
 function clock() { document.body.innerHTML = new Date().toLocaleTimeString(); }
 setInterval(clock, 1000);
+
+function randColor() { return '#' + Math.floor(Math.random()*16777215).toString(16); }
+document.body.style.background = randColor();
