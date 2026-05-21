8bb3e5f970ee475b0719fb0ce99d84e5eb24cf98
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 17:42:00 +0700

    [ASYNC] Fetch current weather data

--- BTTH03_javascript/projects/03_advanced/weather.js
+++ BTTH03_javascript/projects/03_advanced/weather.js
@@ -1,1 +1,3 @@
 const WEATHER_API = 'KEY'; async function getWeather(city) {}
+
+async function getWeather(city) { const res = await fetch(pi.openweathermap.org/data/2.5/weather?q=+city); return await res.json(); }
