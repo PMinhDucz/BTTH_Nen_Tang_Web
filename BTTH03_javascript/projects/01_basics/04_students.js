23d6badb9f0631a1eddcfe2d32f747e5c11962a1
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 15:56:00 +0700

    [CRUD] Add function to delete student

--- BTTH03_javascript/projects/01_basics/04_students.js
+++ BTTH03_javascript/projects/01_basics/04_students.js
@@ -3,3 +3,5 @@ const students = [{id:1, name:'Duc', score:9}, {id:2, name:'Lan', score:8}];
 function render() { console.log(students); }
 
 function addStudent(name, score) { students.push({id: Date.now(), name, score}); }
+
+function delStudent(id) { const idx = students.findIndex(s => s.id === id); students.splice(idx, 1); }
