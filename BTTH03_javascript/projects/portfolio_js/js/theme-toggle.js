66c1033b8074b0fa3d27c2dfde5e30b64141772c
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 16:41:00 +0700

    [STATE] Create theme state management

--- BTTH03_javascript/projects/portfolio_js/js/theme-toggle.js
+++ BTTH03_javascript/projects/portfolio_js/js/theme-toggle.js
@@ -1,2 +1,11 @@
 const themeToggleBtn = document.getElementById('theme-toggle');
 const htmlElement = document.documentElement;
+
+function setTheme(theme) {
+    htmlElement.setAttribute('data-theme', theme);
+    if (theme === 'dark') {
+        themeToggleBtn.classList.add('dark-mode');
+    } else {
+        themeToggleBtn.classList.remove('dark-mode');
+    }
+}
