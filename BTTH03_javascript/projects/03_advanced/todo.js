f665e48d4fc8e14e8289c00dc43459643d109375
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 16:59:00 +0700

    [FEATURE] Implement toggle complete status

--- BTTH03_javascript/projects/03_advanced/todo.js
+++ BTTH03_javascript/projects/03_advanced/todo.js
@@ -3,3 +3,5 @@ let todos = [];
 function addTodo(text) { todos.push({id: Date.now(), text, done: false}); }
 
 function deleteTodo(id) { todos = todos.filter(t => t.id !== id); }
+
+function toggleTodo(id) { const todo = todos.find(t => t.id === id); if(todo) todo.done = !todo.done; }
