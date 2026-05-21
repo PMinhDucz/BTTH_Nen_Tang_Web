2487013ca20e5bd6a59e745bf2ce15aa1438aec4
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 15:26:00 +0700

    [EVENT] Handle real-time input events

--- BTTH03_javascript/projects/01_basics/02_events.js
+++ BTTH03_javascript/projects/01_basics/02_events.js
@@ -1,2 +1,5 @@
 const btn = document.getElementById('btn');
 btn.addEventListener('click', () => alert('Clicked'));
+
+const input = document.getElementById('input');
+input.addEventListener('input', (e) => console.log(e.target.value));
