907ba4b7b56edb541286b1bf229df332e09687ab
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 17:48:00 +0700

    [FEATURE] Add HTML5 Geolocation support

--- BTTH03_javascript/projects/03_advanced/weather.js
+++ BTTH03_javascript/projects/03_advanced/weather.js
@@ -3,3 +3,5 @@ const WEATHER_API = 'KEY'; async function getWeather(city) {}
 async function getWeather(city) { const res = await fetch(pi.openweathermap.org/data/2.5/weather?q=+city); return await res.json(); }
 
 function renderWeather(data) { document.body.style.background = data.main.temp > 30 ? 'orange' : 'blue'; }
+
+navigator.geolocation.getCurrentPosition(pos => getWeatherByCoords(pos.coords.latitude, pos.coords.longitude));
