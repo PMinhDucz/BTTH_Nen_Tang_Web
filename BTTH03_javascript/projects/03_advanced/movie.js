be9dd3d6f8e568350d099ed2b438d50e7b78f62d
PMinhDucz <phamminhduc141@gmail.com>
2026-05-21 17:14:00 +0700

    [UI] Create movie card rendering logic

--- BTTH03_javascript/projects/03_advanced/movie.js
+++ BTTH03_javascript/projects/03_advanced/movie.js
@@ -1,3 +1,5 @@
 const API_KEY = 'YOUR_KEY'; async function searchMovie(query) {}
 
 async function searchMovie(query) { const res = await fetch(http://www.omdbapi.com/?s=+query); const data = await res.json(); return data; }
+
+function renderMovies(movies) { console.log('Rendering', movies.length, 'movies'); }
